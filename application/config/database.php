34d0d981b00278c0e936555953deda5f1815395b
Kyungbae Kim <kyungbaekim@gmail.com>
2016-04-22 15:00:48 -0700

    config file modified

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '127.0.0.1';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'k1980222';
 $db['default']['database'] = 'travel';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
